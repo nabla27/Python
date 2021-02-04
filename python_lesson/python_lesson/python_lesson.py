bcf25959965f4b48d074546e228fd51fa5feb28d
nabla <nabla3.1415@gmail.com>
2021-02-04 18:17:55 +0900

    記述を見やすく変更

--- python_lesson/python_lesson/python_lesson.py
+++ python_lesson/python_lesson/python_lesson.py
@@ -2,33 +2,42 @@
 #対応するC言語での記述はクオーテーション内で記述
 
 
-#############################
+###############################################
 #初期的な設定(モジュールなど)
-#############################
-import numpy										#モジュールをインポート
-import matplotlib.pyplot as plt						#モジュール名に別名をつけてインポート
-from collections import defaultdict, Counter		#モジュール内の特定の機能だけをインポート
-													#モジュールの関数を使うときは、関数の前に"モジュール名."をつける
+###############################################
+
+#モジュールをインポート
+import numpy					
+#モジュール名に別名をつけてインポート
+import matplotlib.pyplot as plt						
+#モジュール内の特定の機能だけをインポート
+from collections import defaultdict, Counter		
+#モジュールの関数を使うときは、関数の前に"モジュール名."をつける
 
 """
-include <iostream>									#ヘッダファイルをインクルード
-using namespace std;								#スコープ解決演算子を用いる名前空間名の省略
+#ヘッダーファイルのインクルード
+include <iostream>				
+#スコープ解決演算子を用いる名前空間名の省略
+using namespace std;								
 """
 
-###########
+###############################################
 #関数の定義
-###########
-def double(x):										#def <関数名>: で宣言
-	return x * 2									#return で返す
+###############################################
 
+#"def <関数名>:"で宣言。"return"で値を返す
+def double(x):										
+	return x * 2									
+#関数fを変数とする関数apply
 def apply(f):
-	f(1)											#
-
+	f(1)											
+#関数である変数my_doubleをapply関数に代入
 my_double = double
 x = apply(my_double)
 
 """
-int double(x) {										#必ず戻り値の型名を宣言
-	return x * 2;									#セミコロンを忘れずに
+#必ず型名を宣言する。セミコロンを忘れずにする。
+int double(x) {										
+	return x * 2;									
 }
 """
