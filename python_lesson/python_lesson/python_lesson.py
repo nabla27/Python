aad38acf379119f86e7af5b3e0e7d73d8d910a29
nabla <nabla3.1415@gmail.com>
2021-02-04 18:50:44 +0900

    関数について

--- python_lesson/python_lesson/python_lesson.py
+++ python_lesson/python_lesson/python_lesson.py
@@ -26,18 +26,45 @@ using namespace std;
 ###############################################
 
 #"def <関数名>:"で宣言。"return"で値を返す
-def double(x):										
-	return x * 2									
+def double(x):	
+	return x * 2
+
 #関数fを変数とする関数apply
 def apply(f):
-	f(1)											
+	f(1)
+	
 #関数である変数my_doubleをapply関数に代入
 my_double = double
 x = apply(my_double)
 
+#名前を持たない短い関数(ラムダ式)を宣言。"lambda <変数名>:式"
+y = apply(lambda x: x + 4)
+
+#デフォルト引数を持つ関数。引数を指定しないときに、デフォルトの値が渡される。
+def my_print(string="defalt message"):
+	print(string)
+
+#defalt message と出力される
+my_print()
+
+
 """
-#必ず型名を宣言する。セミコロンを忘れずにする。
-int double(x) {										
-	return x * 2;									
+#Cでは上記のようにいきなり変数x,yを出してはならない。必ず型名とともに宣言してから使う。
+#仮引数は宣言の必要なし。ただし、仮引数に型名の指定が必要。
+int y;
+
+#必ず型名を宣言する。セミコロンを忘れずにする。仮引数の型名も忘れずに。
+int double(int x) {
+	return x * 2;
+}
+
+#インライン関数の宣言。"inline <型名> <関数名>(型名 引数){};"
+#短い処理のみ可。呼び出し部分に埋め込まれ、処理速度向上。
+inline int apply(int x){x + 4};
+
+#デフォルト引数を持つ関数。プロトタイプ宣言時にも、通常の関数宣言時にも指定可。
+int number(int x = 4);
+int number(int x){
+	return x + 4;
 }
 """
